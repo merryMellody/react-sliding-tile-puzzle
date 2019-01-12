5a388016a54ee2d927a01edd50509bd840f553ed
AmauriAires <amauriairesfilho@gmail.com>
2019-01-12 12:04:20 -0500

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class SlidingTilePuzzle extends React.Component {
+    render() {
+        return <div>This is a very cool Sliding Tile Puzzle!</div>
+    }
+}
+export default SlidingTilePuzzle
