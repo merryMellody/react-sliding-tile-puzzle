e1f71ca0fb76d4a1d6088eaf143d7ba9cc8fffc4
amauriairesfilho <amauriairesfilho@gmail.com>
2019-02-13 01:26:39 -0500

    Updates

---
+++ precache-manifest.108dbe13e741ff9b9e84d3e3b1b70dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91fb3e17793afb9c3475",
+    "url": "/react-sliding-tile-puzzle/sample/static/js/runtime~main.91fb3e17.js"
+  },
+  {
+    "revision": "fb5c67fc16c55045a2af",
+    "url": "/react-sliding-tile-puzzle/sample/static/js/main.fb5c67fc.chunk.js"
+  },
+  {
+    "revision": "67cfc30b66408487d0e2",
+    "url": "/react-sliding-tile-puzzle/sample/static/js/1.67cfc30b.chunk.js"
+  },
+  {
+    "revision": "fb5c67fc16c55045a2af",
+    "url": "/react-sliding-tile-puzzle/sample/static/css/main.74f127ba.chunk.css"
+  },
+  {
+    "revision": "8fa057821bcdcdac1174c2e782d2a67a",
+    "url": "/react-sliding-tile-puzzle/sample/index.html"
+  }
+];
