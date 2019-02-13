f69b8a97d77fc5ac027c13c11a781955b51da083
amauriairesfilho <amauriairesfilho@gmail.com>
2019-02-13 00:13:44 -0500

    Reestruture root folder

---
+++ sample/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./App.css";
+import SlidingTilePuzzle from "react-sliding-tile-puzzle";
+import { Button, Segment } from "semantic-ui-react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      solvePuzzle: false,
+      newPuzzle: false
+    };
+  }
+
+  handleClickSolvePuzzle = () => {
+    this.setState({ solvePuzzle: !this.state.solvePuzzle });
+  };
+
+  handleClickNewPuzzle = () => {
+    this.setState({ newPuzzle: !this.state.newPuzzle });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Segment.Group>
+          <Segment>Slidding Tile Puzzle</Segment>
+          <Segment.Group horizontal>
+            <Segment.Group vertical="true">
+              <Segment>
+                <Button fluid onClick={this.handleClickNewPuzzle}>
+                  New Puzzle
+                </Button>
+              </Segment>
+              <Segment>
+                <Button fluid onClick={this.handleClickSolvePuzzle}>
+                  Solve Puzzle
+                </Button>
+              </Segment>
+            </Segment.Group>
+            <Segment>
+              <SlidingTilePuzzle
+                solvePuzzle={this.state.solvePuzzle}
+                newPuzzle={this.state.newPuzzle}
+                maxIterations={100}
+              />
+            </Segment>
+          </Segment.Group>
+        </Segment.Group>
+      </div>
+    );
+  }
+}
+
+export default App;
